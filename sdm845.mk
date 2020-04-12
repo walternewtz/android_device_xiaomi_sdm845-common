e4ac2a5b678094870335d863753d8f27fab4bcc8
jjpprrrr <zhongcy95@gmail.com>
2020-04-12 13:28:03 +0300

    sdm845-common: Remove TZ version assertion and move to in-built firmware Co-authored-by: Dyneteve <dyneteve@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -272,10 +272,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OPScreenRecorder
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_xiaomi
-
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
