1e12961c0565ad588c3ae1c014bd04e753944622
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    sdm845-common: Move to common IFAAService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2

--- sdm845.mk
+++ sdm845.mk
@@ -205,11 +205,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-#PRODUCT_PACKAGES += \
-#    org.ifaa.android.manager
-
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_PACKAGES += \
+    IFAAService
 
 # Input
 PRODUCT_COPY_FILES += \
