75e0438e2cc457ab9840998270f9693983ba98db
Lup Gabriel <gwolf2u.website@gmail.com>
2022-10-23 10:09:35 +0300

    sdm845-common: enable blur

--- sdm845.mk
+++ sdm845.mk
@@ -391,5 +391,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
